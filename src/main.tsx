46855657483812b2b6a5ab658e6198e8578f0b0e
RomainDoyen <romain.doyen97@gmail.com>
2025-09-15 21:27:16 +0400

    Création de la page détail d'un jeu et ajout aux favoris

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import Games from './pages/Games.tsx'
+import GameDetails from './pages/GameDetails.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/games" element={<Games />} />
+      <Route path="/games/:id" element={<GameDetails />} />
     </Routes>
   </BrowserRouter>,
 )
