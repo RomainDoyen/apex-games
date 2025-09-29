11b898566077f8745696e095d229177d85f26e4c
Bobe974 <etienne.baillif97436@gmail.com>
2025-09-29 15:33:13 +0200

    ajout des bouttons d'actions pour une card du backlog

--- apex-games-frontend/src/components/ui/MovableCard.tsx
+++ apex-games-frontend/src/components/ui/MovableCard.tsx
@@ -4,14 +4,46 @@ import type { MovableCardProps } from "../../types/types";
 
 export default function MovableCard({id, title, image }: MovableCardProps) {
     return (
-        <div className="movableCard"draggable onDragStart={(e)=>{
-            e.dataTransfer.setData('gameId',id.toString());
-        }}>
+        <div 
+            className="movableCard" 
+            draggable 
+            onDragStart={(e) => {
+                e.dataTransfer.setData('gameId',id.toString());
+            }}
+        >
+            <div 
+                className="movableCard-delete-button" 
+                //onClick={handleDeleteClick} 
+                title="Supprimer la carte" 
+            >
+                &times; 
+            </div>
+            
             <div className="movableCard-image">
                 <Image src={image} alt={title} className="movableCard-image" />
             </div>
+            
+            {/* Contenu qui apparaît au survol */}
             <div className="movableCard-content">
                 <h3>{title}</h3>
+                
+                <div className="card-floating-actions"> 
+                    <div 
+                        className="action-icon info-icon"
+                        //onClick={handleInfoClick} 
+                        title="Détails du jeu"
+                    >
+                        &#x1F6C8; {/* Info */}
+                    </div>
+                    
+                    <div 
+                        className="action-icon review-icon"
+                        //onClick={handleReviewClick} 
+                        title="Donner un avis"
+                    >
+                        &#9733; {/* Avis */}
+                    </div>
+                </div>
             </div>
         </div>
     )
